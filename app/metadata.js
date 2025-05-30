c503f25269819c7d26d36bb2b8f41e3404b0cf6c
Jeff Doan <jeffdoan@Kizaans-MacBook-Pro.local>
2025-05-30 13:00:13 -0600

    Add in netlify.toml file

---
+++ app/metadata.js
@@ -0,0 +1,14 @@
+// page title / description
+export const metadata = {
+  title: 'My Chorelist',
+  description: 'A simple chore list app to help you stay organized',
+  openGraph: {
+    title: 'My ChoreList: A simple way to stay on track, for you and your kiddos',
+    description: 'Track your daily and weekly chores, free. No accounts, 100% private.',
+    images: ['/images/social-image.png']
+  },
+  icons: {
+    icon: '/images/logo--holder.png',
+    apple: '/images/logo--holder.png'
+  }
+};
